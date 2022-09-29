69a1b3e17222267edb6cb3f8cdd9dc008133dcab
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-09-29 10:12:19 +0700

    Tipe data primitif pada dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,16 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:fasttrack_app/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  // string datatype
+  String name = "ACENG".toLowerCase();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  // number datatype (num, int and double)
+  num familyMembers = 6;
+  int age = 18;
+  double salary = 2.500;
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  // boolean datatype
+  bool isHappy = false;
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  print("Is $name feels happy? $isHappy");
+  print("And now $name is $age years old.");
+  print("He have salary that the total is ${salary.abs()}");
 }
