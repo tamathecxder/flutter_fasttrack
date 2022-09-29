f9696d9ce819bf49c62d665a316fea5f57343c20
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-09-29 10:53:26 +0700

    Function pada dart

---
+++ test/function.dart
@@ -0,0 +1,14 @@
+void main() {
+  sayHello();
+
+  int sumResult = sumTwoNumbers(5, 5);
+  print(sumResult);
+}
+
+void sayHello() {
+  print("Hello, World");
+}
+
+int sumTwoNumbers(int x, int y) {
+  return x + y;
+}
