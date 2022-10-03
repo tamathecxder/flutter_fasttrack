820e263a342f41433857108fa839c22daac74c24
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-10-03 22:49:37 +0700

    Membuat form input sederhana dengan widget TextField

--- lib/main.dart
+++ lib/main.dart
@@ -9,43 +9,104 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool isHidden = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter SnackBar"),
+        title: Text("TextField Flutter"),
         centerTitle: true,
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text("Lorem ipsum dolor sit amet!"),
-                action: SnackBarAction(
-                  label: "CANCEL",
-                  onPressed: () {
-                    print('canceled');
-                  },
-                ),
-                backgroundColor: Colors.black,
-                duration: Duration(seconds: 2),
-                margin: EdgeInsets.all(20),
-                behavior: SnackBarBehavior.floating,
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          TextField(
+            controller: emailController,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(
+              label: Text("Email Address:"),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
               ),
-            );
-          },
-          child: Text("Show SnackBar"),
-        ),
+              prefixIcon: Icon(Icons.mail),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            controller: passwordController,
+            autocorrect: false,
+            obscureText: isHidden,
+            keyboardType: TextInputType.text,
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+              label: Text("Password"),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              prefixIcon: Icon(Icons.vpn_key_sharp),
+              suffixIcon: IconButton(
+                onPressed: () {
+                  if (isHidden == true) {
+                    isHidden = false;
+                  } else {
+                    isHidden = true;
+                  }
+
+                  setState(() {});
+                },
+                icon: isHidden
+                    ? Icon(Icons.visibility_off)
+                    : Icon(Icons.remove_red_eye),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text("Terautentikasi!"),
+                  content: Text("Login sebagai ${emailController.text}"),
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: Text("Test"),
+                    ),
+                  ],
+                ),
+              );
+            },
+            child: Text("LOG IN"),
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.all(16),
+            ),
+          ),
+        ],
       ),
     );
   }
