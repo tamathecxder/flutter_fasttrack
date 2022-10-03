b31185d1c73b4a91f6f9d1fb0fa43591485cf901
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-10-03 16:28:10 +0700

    Membuat studi kasus counter apps dengan stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,69 @@
-import 'package:fasttrack_app/widgets/widget_page_1.dart';
-import 'package:fasttrack_app/widgets/widget_part_10.dart';
-import 'package:fasttrack_app/widgets/widget_part_11.dart';
-import 'package:fasttrack_app/widgets/widget_part_12.dart';
-import 'package:fasttrack_app/widgets/widget_part_13.dart';
-import 'package:fasttrack_app/widgets/widget_part_14.dart';
-import 'package:fasttrack_app/widgets/widget_part_15.dart';
-import 'package:fasttrack_app/widgets/widget_part_16.dart';
-import 'package:fasttrack_app/widgets/widget_part_17.dart';
-import 'package:fasttrack_app/widgets/widget_part_18.dart';
-import 'package:fasttrack_app/widgets/widget_part_19.dart';
-import 'package:fasttrack_app/widgets/widget_part_2.dart';
-import 'package:fasttrack_app/widgets/widget_part_20.dart';
-import 'package:fasttrack_app/widgets/widget_part_21.dart';
-import 'package:fasttrack_app/widgets/widget_part_22.dart';
-import 'package:fasttrack_app/widgets/widget_part_23.dart';
-import 'package:fasttrack_app/widgets/widget_part_24.dart';
-import 'package:fasttrack_app/widgets/widget_part_3.dart';
-import 'package:fasttrack_app/widgets/widget_part_4.dart';
-import 'package:fasttrack_app/widgets/widget_part_5.dart';
-import 'package:fasttrack_app/widgets/widget_part_6.dart';
-import 'package:fasttrack_app/widgets/widget_part_7.dart';
-import 'package:fasttrack_app/widgets/widget_part_8.dart';
-import 'package:fasttrack_app/widgets/widget_part_9.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: WidgetPage24(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Increment Apps"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "$counter",
+                style: TextStyle(fontSize: 60),
+              ),
+              Padding(padding: EdgeInsets.all(6)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: <int>() {
+                      counter = counter - 1;
+                      if (counter < 0) {
+                        return counter = 0;
+                      }
+                      print(counter);
+
+                      setState(() {});
+                    },
+                    child: Text("-"),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      counter = counter + 1;
+                      print(counter);
+
+                      setState(() {});
+                    },
+                    child: Text("+"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
