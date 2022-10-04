7ae90f0f50ee45766479b4702d835f5a3a701d84
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-10-04 13:41:41 +0700

    Bottom Sheet pada Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -15,65 +15,73 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late int currentBtNav;
-  List<Widget> showCurrentView = [
-    Center(
-      child: Text("Homepage"),
-    ),
-    Center(
-      child: Text("Profile"),
-    ),
-    Center(
-      child: Text("Notifications"),
-    ),
-  ];
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    currentBtNav = 0;
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Bottom Navigation"),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurple,
+        title: Text("Bottom Sheet Flutter"),
       ),
-      body: showCurrentView[currentBtNav],
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.deepPurple[400],
-        currentIndex: currentBtNav,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: ElevatedButton(
+            onPressed: () {
+              showModalBottomSheet(
+                isDismissible: false,
+                context: context,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(24.0),
+                  ),
+                ),
+                backgroundColor: Colors.white,
+                builder: (context) {
+                  return Container(
+                    height: 300,
+                    child: ListView(
+                      padding: EdgeInsets.only(top: 16, bottom: 16),
+                      children: [
+                        ListTile(
+                          leading: Icon(Icons.drive_folder_upload_outlined),
+                          title: Text("New Folder"),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.share),
+                          title: Text("Share"),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.replay_outlined),
+                          title: Text("Refresh"),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.more_horiz),
+                          title: Text("More"),
+                        ),
+                        ListTile(
+                          title: ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text(
+                              "Close".toUpperCase(),
+                              style: TextStyle(fontSize: 14),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                                minimumSize: Size.fromHeight(50)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+            child: Text("Show Sheet"),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: "Profile",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications_active),
-            label: "Notifications",
-          ),
-        ],
-        onTap: (value) {
-          setState(() {
-            currentBtNav = value;
-          });
-        },
+        ),
       ),
     );
   }
