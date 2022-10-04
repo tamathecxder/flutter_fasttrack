3773762ec720a12b3f5324b2342958caa04d1b94
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-10-04 09:57:44 +0700

    TabBar widget pada flutter

--- lib/main.dart
+++ lib/main.dart
@@ -22,89 +22,61 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  bool isHidden = true;
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late TabController chatCtr = TabController(length: 4, vsync: this);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("TextField Flutter"),
-        centerTitle: true,
+        title: Text(
+          "WhatsApp",
+          style: TextStyle(fontSize: 20),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_vert),
+          ),
+        ],
+        centerTitle: false,
+        backgroundColor: Colors.teal[800],
+        bottom: TabBar(
+          controller: chatCtr,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              text: "Chats".toUpperCase(),
+            ),
+            Tab(
+              text: "Status".toUpperCase(),
+            ),
+            Tab(
+              text: "Calls".toUpperCase(),
+            ),
+          ],
+        ),
       ),
-      body: ListView(
-        padding: EdgeInsets.all(16),
+      body: TabBarView(
+        controller: chatCtr,
         children: [
-          TextField(
-            controller: emailController,
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            textInputAction: TextInputAction.next,
-            decoration: InputDecoration(
-              label: Text("Email Address:"),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-              prefixIcon: Icon(Icons.mail),
-            ),
+          Center(
+            child: Text("Camera"),
           ),
-          SizedBox(
-            height: 20,
+          Center(
+            child: Text("Chats"),
           ),
-          TextField(
-            controller: passwordController,
-            autocorrect: false,
-            obscureText: isHidden,
-            keyboardType: TextInputType.text,
-            textInputAction: TextInputAction.done,
-            decoration: InputDecoration(
-              label: Text("Password"),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-              prefixIcon: Icon(Icons.vpn_key_sharp),
-              suffixIcon: IconButton(
-                onPressed: () {
-                  if (isHidden == true) {
-                    isHidden = false;
-                  } else {
-                    isHidden = true;
-                  }
-
-                  setState(() {});
-                },
-                icon: isHidden
-                    ? Icon(Icons.visibility_off)
-                    : Icon(Icons.remove_red_eye),
-              ),
-            ),
+          Center(
+            child: Text("Status"),
           ),
-          SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: Text("Terautentikasi!"),
-                  content: Text("Login sebagai ${emailController.text}"),
-                  actions: [
-                    ElevatedButton(
-                      onPressed: () => Navigator.pop(context),
-                      child: Text("Test"),
-                    ),
-                  ],
-                ),
-              );
-            },
-            child: Text("LOG IN"),
-            style: ElevatedButton.styleFrom(
-              padding: EdgeInsets.all(16),
-            ),
+          Center(
+            child: Text("Calls"),
           ),
         ],
       ),
